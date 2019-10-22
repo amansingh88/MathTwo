63125ccb904f9f3bdcec7b872b2505e86a276a30
amansingh88 <singhamanpreet.dc@gmail.com>
2019-10-22 10:10:05 -0400

    Math Two exercise

---
+++ src/MathTwo.java
@@ -0,0 +1,13 @@
+/*
+Write an application that assigns 2 to a variable named mynumber and 1.7938 to a variable namedmyothernumber,
+and then prints out the values to the console.
+ */
+
+public class MathTwo {
+    public static void main(String[] args){
+        int mynumber = 2;
+        double myothernumber = 1.7938;
+        System.out.println("The value of mynumber is: " +mynumber);
+        System.out.println("The value of myothernumber is: " +myothernumber);
+    }
+}
